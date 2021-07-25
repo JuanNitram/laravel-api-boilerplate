0d37d0ef377f631b8a9bcebf78b1a15efd154055
Juan Vargas <juanmvg2006@gmail.com>
2021-07-25 18:10:26 -0300

    Update api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
